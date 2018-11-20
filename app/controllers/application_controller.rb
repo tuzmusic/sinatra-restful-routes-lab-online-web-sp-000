da397b9b78da98c4f378f1b0fc233788fd6324ea
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-20 19:09:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    # displays the list of recipes
+    # with links to each recipe's show page
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    # displays the recipe
+    # with a button (in its own form) to delete the recipe
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    # displays the recipe and an edit form
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    # edits the recipe
+  end
 
 end
